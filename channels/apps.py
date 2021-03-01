78f9e387f4249e44a2562af7c3096ecf8b2c26e0
Christofer Bertonha <christofer.bertonha@typeform.com>
2021-03-01 15:05:38 +0100

    Dont load daphne

--- channels/apps.py
+++ channels/apps.py
@@ -1,10 +1,4 @@
-# We import this here to ensure the reactor is installed very early on
-# in case other packages accidentally import twisted.internet.reactor
-# (e.g. raven does this).
-import daphne.server
 from django.apps import AppConfig
-
-assert daphne.server  # pyflakes doesn't support ignores
 
 
 class ChannelsConfig(AppConfig):
